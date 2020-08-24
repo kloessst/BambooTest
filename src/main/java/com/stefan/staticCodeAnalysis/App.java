0acfccfb9d042e360f2dd5280e26cfdb3bbc34ea
Stefan Klöss-Schuster <16407766+kloessst@users.noreply.github.com>
2020-08-24 12:27:29 +0200

    Update App.java

--- src/main/java/com/stefan/staticCodeAnalysis/App.java
+++ src/main/java/com/stefan/staticCodeAnalysis/App.java
@@ -5,7 +5,7 @@ package com.stefan.staticCodeAnalysis;
  *
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args)
         String a = "A";
         String b = "B";
         System.out.println(equalString("Hello"));
