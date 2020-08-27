10f7c68620f7d61002ec08ec109f94afaf1b5ba4
Stefan Klöss-Schuster <16407766+kloessst@users.noreply.github.com>
2020-08-27 15:26:51 +0200

    Create TestCompile.java

---
+++ src/main/TestCompile.java
@@ -0,0 +1,12 @@
+public class Test {
+  public static void testingThis() {
+    String a = "a";
+    String b = "b";
+    compareString(a);
+  }
+  
+  public boolean compareString(String a) {
+    return a == "b";
+  }
+  
+}
